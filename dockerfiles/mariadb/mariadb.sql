8897f10a948832f4e99b909c3fdcd407f8481a8c
root <root@mediawiki.publicsubnet100.testvcn.oraclevcn.com>
2020-09-02 07:53:54 +0000

    Final

--- dockerfiles/mariadb/mariadb.sql
+++ dockerfiles/mariadb/mariadb.sql
@@ -1,4 +1,4 @@
-mysql -u root -pabcd1234 <<EOF
+mysql  <<EOF
 CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'abcd1234';
 CREATE DATABASE wikidatabase;
 GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';
