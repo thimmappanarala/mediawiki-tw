ac234fa8562e28567bfb294b33ede2bb74e470f6
root <root@mediawiki.publicsubnet100.testvcn.oraclevcn.com>
2020-09-02 07:30:37 +0000

    Changes

--- terraform/oci-compute-mediawiki/export.sh
+++ terraform/oci-compute-mediawiki/export.sh
@@ -1,6 +1,6 @@
-export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaa6hqeaaiu4j6osmqi3e4e5ncq7e7sa5ydzzd33bquycny3rbxvwpq
-export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaapbcdhrr7mqcpqkwhpr7ia74wwrubfxzkah45roeymhav2ilqo77a
+export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaa3wlcifoq6kyl4wmfft7xm7ssp4oz67qxl4lulqvpukchlenat5cq
+export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaat7cwfpl4sd7ymlgnre7ybl3hjfb4x6wmgvwx2wohcpxbkjxh7jta
 export TF_VAR_fingerprint=f9:ea:2d:0a:6a:b4:6f:bc:cf:ae:3b:c9:14:dd:81:51
 export TF_VAR_region=us-ashburn-1
-export TF_VAR_private_key_path=/root/terraform/oci-compute-mediawiki/thims_api_key.pem
-export TF_VAR_compartment_ocid=ocid1.compartment.oc1..aaaaaaaa4xosvfl2h2lu3qsay7sx5eh7yvg7gt7lg456tffupqggnqbxm3ea
+export TF_VAR_private_key_path=thims_api_key.pem
+export TF_VAR_compartment_ocid=ocid1.compartment.oc1..aaaaaaaakqlagjzxpzm54gxnuwqgkhm5pnrmtuolzs6lnhhi2qd2qnmbqkzq
